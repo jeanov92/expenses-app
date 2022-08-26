83d0e6a561970e6e104d1b56c7dfa514fae1c0d8
Jean Carlo Ovalles <jeanov92@gmail.com>
2022-08-26 17:21:56 -0500

    Adding an ID to the expense data

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,8 +3,12 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense(props) {
-  const newExpenseHandler = (expense) => {
-    props.onAddExpense(expense);
+  const newExpenseHandler = (enteredExpenseData) => {
+    const expenseData = {
+        ...enteredExpenseData,
+        id: Math.floor(Math.random() * 1000)
+    }
+    props.onAddExpense(expenseData);
   };
 
   return (
